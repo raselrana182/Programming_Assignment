6e56eece6a9d0f79346b0880b97ef81826a66f61
raselrana182 <raselrana182@gmail.com>
2020-07-17 23:46:59 +0600

    Completed

--- assignment.js
+++ assignment.js
@@ -1,25 +1,27 @@
-// function feetToMile(feet){
-//     var mile = feet/5280;
-//     if(feet < 0 ){
-//         return "Distance can't be a negative value."
-//     }
-//     return mile;
-    
-// }
-// var result = feetToMile(1000);
-// console.log(result);
+// feetToMile //
 
-// function woodCalculator(chair,table,bed){
-//     var woodForChair = chair*1;
-//     var woodForTable = table*3;
-//     var woodForBed = bed*5;
-//     if(chair < 0 || table < 0 || bed < 0){
-//         return "Qunatity never be a negative number."
-//     }
-//     return woodForChair + woodForTable + woodForBed;
-// }
-// var TotalWood = woodCalculator(1,2,3);
-// console.log(TotalWood);
+function feetToMile(feet){
+    var mile = feet/5280;
+    if(feet < 0 ){
+        return "Distance can't be a negative value."
+    }
+    return mile;
+}
+
+// woodCalculator //
+
+function woodCalculator(chair,table,bed){
+    var woodForChair = chair*1;
+    var woodForTable = table*3;
+    var woodForBed = bed*5;
+    if(chair < 0 || table < 0 || bed < 0){
+        return "Qunatity never be a negative number."
+    }
+    return woodForChair + woodForTable + woodForBed;
+}
+
+
+// brickCalculator //
 
 function brickCalculator(floors){
     if (floors>=1 && floors<=10){
@@ -27,10 +29,9 @@ function brickCalculator(floors){
             return "Floor's number never be a negative value"
         }
         for(i = 0; i<=floors; i++){
-            brick = 1000*15*floors;
-            // total1 = brick1*floors;
+            var firstTenFloor = 1000*15*floors;
         }
-        return brick;
+        return firstTenFloor;
     }
 
     else if(floors>=11 && floors<=20){
@@ -38,34 +39,32 @@ function brickCalculator(floors){
             return "Floor's number never be a negative value"
         }
         for(i = 11; i<=floors; i++){
-            brick = (1000*15*10) + 1000*12*(floors-10);
-            //total2 = brick2*floors;
+            var secondTenFloor = (1000*15*10) + 1000*12*(floors-10);
         }
-        return brick;
+        return secondTenFloor;
     }
     else{
         if(floors < 0){
             return "Floor's number never be a negative value"
         }
         for(i = 21; i<=floors; i++){
-            brick =(1000*15*10) + (1000*12*10) + 1000*10*(floors-20);
-            // total3 = brick3*floors;
+            var aboveFloor =(1000*15*10) + (1000*12*10) + 1000*10*(floors-20);
         }
-        return brick;
+        return aboveFloor;
     }
 }
-var result = brickCalculator(22);
-console.log(result);
 
-// function tinyFriend(str){
-//     var friendName = '';
-//     for(i = 0; i<str.length; i++){
-//         var smallestName = str[i];
-//         if (smallestName < friendName){
-//             smallestName = friendName;
-//         }
-//     }
-//     return smallestName;
-// }
-// var result = tinyFriend(['Arko', 'Rashedul', 'Mishu', 'Akhi']);
-// console.log(result);
+
+// tinyFriend //
+
+function tinyFriend(str){
+    var friendName = '';
+    for(i = 0; i<str.length; i++){
+        var smallestName = str[i];
+        if (smallestName.length < friendName.length){
+            smallestName = friendName;
+        }
+    }
+    return smallestName;
+}
+
